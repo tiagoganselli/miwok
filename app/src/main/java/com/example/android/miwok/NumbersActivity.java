7f6538366b1ed58b8b3a2aa2b4961ee53250aede
Tiago Ganselli <ganselli@motorola.com>
2017-09-14 13:01:20 -0300

    Add arrayList of strings and loop to create TextViews.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.i("miwok", "Size is " + words.size());
+
+        LinearLayout viewNumbers = (LinearLayout)findViewById(R.id.activity_numbers);
+
+        for (int i=0; i<10; i++) {
+            TextView viewWord = new TextView(this);
+            viewWord.setText(words.get(i));
+            viewNumbers.addView(viewWord);
+        }
+
     }
 }
